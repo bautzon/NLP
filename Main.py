3f9d6f978a57fd309d30176d6553c41b4ff3a7fd
Gome <82088992+zaibot123@users.noreply.github.com>
2022-03-19 16:54:59 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,19 @@
+from Scraper import *
+from Cleaner import*
+
+
+
+
+choose_method()   #scraper
+print("Before", data)
+print("""
+
+
+
+
+""")
+
+cleaned_text_list=cleaner(data['title'][0])
+cleaned_text_string=cleaner(data['title'][1])
+
+
